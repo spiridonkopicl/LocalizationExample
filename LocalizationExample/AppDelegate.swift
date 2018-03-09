4fd77442392ea886f2564a6396a3b02287d8f21a
Predrag Karic <predrag.karic@codetri.be>
2018-03-09 10:54:24 +0100

    # Please include a useful commit message!

--- LocalizationExample/AppDelegate.swift
+++ LocalizationExample/AppDelegate.swift
@@ -18,10 +18,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
