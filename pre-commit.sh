72347e146e6f1b49b2cd275a613588893755f824
Predrag Karic <predrag.karic@codetri.be>
2018-03-09 15:25:10 +0100

    # Please include a useful commit message!

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 echo "pre-commit started"
-for i in `find ./LocalizationExample/NewRepo/ -name 'Localizable.strings'` ; do echo $i ; done
-
-plutil -lint LocalizableA.strings
+for i in `find ./LocalizationExample/NewRepo -name 'Localizable.strings'` ;
+do echo $i ;
+plutil -lint $i ;
+if [ $? -eq 0 ]
+then
+   echo "It worked"
+else
+   echo "It failed"
+   exit 1
+fi
+done
 
 echo "pre-commit finished"
